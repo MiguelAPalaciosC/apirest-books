842e2252286bfee3b1efe481a29182e461565d2d
MiguelAPalaciosC <a.mapc49@gmail.com>
2024-07-16 18:44:57 -0500

    Usuarios y roles con base de datos

--- src/main/java/com/company/books/backend/config/ConfigSecurity.java
+++ src/main/java/com/company/books/backend/config/ConfigSecurity.java
@@ -1,5 +1,7 @@
 package com.company.books.backend.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,11 +10,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ConfigSecurity {
 
+	/*
 	@Bean
 	public InMemoryUserDetailsManager userDetailsManager() {
 		UserDetails miguel = User.builder()
@@ -34,17 +39,22 @@ public class ConfigSecurity {
 				.build();
 
 		return new InMemoryUserDetailsManager(miguel, agustin, edita);
+	}*/
+	
+	@Bean
+	public UserDetailsManager userDetailsManager(DataSource datasource) {
+		return new JdbcUserDetailsManager(datasource);
 	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(configure -> {
 			configure
-			.requestMatchers(HttpMethod.GET, "/v1/libros").hasRole("Empleado")
-			.requestMatchers(HttpMethod.GET, "/v1/libros/**").hasRole("Empleado")
-			.requestMatchers(HttpMethod.POST, "/v1/libros").hasRole("Jefe")
-			.requestMatchers(HttpMethod.PUT, "/v1/libros/**").hasRole("Jefe")
-			.requestMatchers(HttpMethod.DELETE, "/v1/libros/**").hasRole("Jefe");
+			.requestMatchers(HttpMethod.GET, "/v1/libros").hasRole("EMPLEADO")
+			.requestMatchers(HttpMethod.GET, "/v1/libros/**").hasRole("EMPLEADO")
+			.requestMatchers(HttpMethod.POST, "/v1/libros").hasRole("JEFE")
+			.requestMatchers(HttpMethod.PUT, "/v1/libros/**").hasRole("JEFE")
+			.requestMatchers(HttpMethod.DELETE, "/v1/libros/**").hasRole("JEFE");
 		});
 		
 		http.httpBasic(Customizer.withDefaults());
